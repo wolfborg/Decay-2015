c33d2522586c206d1277ec4a908d4631cd3ccfb9
Derek Chaplin <kingderekx@yahoo.com>
2015-10-09 13:31:53 -0400

    Game so far - Menu

---
+++ decay.hs
@@ -0,0 +1,15 @@
+--Decay.hs
+--Superhero Decision Game
+--Author: Derek Chaplin
+
+main = do
+	putStr "Hello World\n"
+	menu
+
+menu = putStr (
+	"Choose a thing:\n" ++
+	"[1] Test\n" ++
+	"[2] Test\n" ++
+	"[3] Test\n"
+	)
+
