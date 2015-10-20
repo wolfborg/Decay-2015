e6ea57971295d3223053d2c3438b48a1595f30b0
Derek Chaplin <kingderekx@yahoo.com>
2015-10-19 22:01:28 -0400

    Added back input

--- decay.hs
+++ decay.hs
@@ -10,28 +10,28 @@ data Menu = Menu {
 } deriving (Show)
 
 
-mainMenu = Menu "Welcome to Decay" [] [startMenu]
+mainMenu = Menu "Welcome to Decay" ["Start"] [startMenu]
 startMenu = Menu "You Win. Play Again?" ["Back"] [mainMenu]
 
 main = do
   printMenu mainMenu
-  x <- getLine
-  let y = choice mainMenu x
-  if y == "Error"
-    then printMenu mainMenu
-    else putStrLn y
-
+  
 
 printMenu menu = do 
   putStrLn (text menu)
   printChoices menu
   putStr "Choice: "
+  x <- getLine
+  let y = choice menu x
+  if y == "Error"
+    then printMenu menu
+    else putStrLn y
 
 printChoices (Menu _ [] _) = return undefined
 printChoices (Menu t (c:cs) rs) = do
   putStrLn c
   printChoices (Menu t cs rs)
 
-choice menu "1" = ""
-choice menu "2" = ""
+choice menu "1" = "test"
+--choice menu "2" = ""
 choice menu x = "Error"
