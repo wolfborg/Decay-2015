350d0ab5619772d4974b1a811373383717f9961e
Derek Chaplin <kingderekx@yahoo.com>
2015-10-20 21:42:34 -0400

    NOW I fixed it. Hopefully.

--- decay.hs
+++ decay.hs
@@ -15,24 +15,24 @@ startMenu = Menu "You Win. Play Again?" ["Back"] [mainMenu]
 
 main = do
   printMenu mainMenu
-  
+
 --prints menu and asks for choice then prints next menu
 printMenu menu = do 
   putStrLn (text menu)
-  let indexes = [1 .. 10]  --now just is an array of numbers just to be used to print out in the menu
-  printChoices menu indexes
-  --putStr "Choice: "
+  printChoices menu (index (choices menu))
+  putStrLn "Choice: "
   --x <- getLine
   --return x  --prints input
 
 --need a way to print and check choice indexes in order to grab response
 --also need a way to check if anything other than those choices is entered
-
+  
 
 printChoices (Menu _ [] _) (i:is) = return undefined --empty list of choices
+printChoices (Menu t (c:cs) rs) [1] = do
+  putStrLn ("[1] " ++ c)
 printChoices (Menu t (c:cs) rs) (i:is) = do  --anything else, prints choices
-  let x = i
-  putStr ("[" ++ (show x) ++ "] ")
+  putStr ("[" ++ (show i) ++ "] ")
   putStrLn c --prints a choice, but we want numbers with it
   printChoices (Menu t cs rs) is  --prints next choice, cutting off the head
 
