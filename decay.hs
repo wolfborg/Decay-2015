f8f4ef9e83d8a6efd755eede7aa182ebfc4be224
Derek Chaplin <kingderekx@yahoo.com>
2015-10-30 12:17:35 -0400

    Moved menus below code

--- decay.hs
+++ decay.hs
@@ -10,19 +10,14 @@ data Menu = Menu {
   responses :: [Menu]
 } deriving (Show)
 
-
-mainMenu = Menu "Welcome to Decay" ["Start"] [menu1]
-endMenu = Menu "You Win! Play Again?" ["Play Again"] [menu1]
-menu1 = Menu "You are a superhero. What is your power?" ["Spider-powers", "Invisibility", "I have a lot of money"] [spiderman1, invisible1, batman1]
-spiderman1 = Menu "You are Spider-Man, or at least a cheap rip-off of him.\nUsing your spider-senses, speed, and webs, you fight crime and deliver pizzas in New York City.\nYou also used to have an Uncle Ben." ["End"] [endMenu]
-invisible1 = Menu "You have the power of invisibility, which could be very dangerous in the wrong hands.\nTry not to get into too much trouble." ["End"] [endMenu]
-batman1 = Menu "You are Batman.\nYou have a lot of money but that can't buy your parents back to life.\nInstead you bought some bat-themed gadgets and clean up the streets of Gotham at night." ["End"] [endMenu]
+type Log = [Int]
 
 main = do
   printMenu mainMenu
 
 --prints menu and asks for choice then prints next menu
 printMenu menu = do 
+  clearScreen
   putStrLn ((text menu) ++ "\n")
   let indexes = index (choices menu)
   putStrLn "Options:"
@@ -41,6 +36,7 @@ makeChoice "Exit" _ _ = return undefined
 makeChoice "EXIT" _ _ = return undefined
 makeChoice "exit" _ _ = return undefined
 makeChoice x menu indexes = do
+  clearScreen
   let y = (read x)
   if y `elem` indexes
     then do
@@ -62,3 +58,33 @@ printChoices (Menu t (c:cs) rs) (i:is) = do  --anything else, prints choices
 index :: [a] -> [Int]
 index x = [1 .. length x]
 
+mainMenu = Menu 
+  "Welcome to Decay"
+  ["Start"]
+  [menu1]
+
+endMenu = Menu
+  "Game Over. Play Again?"
+  ["Play Again"]
+  [menu1]
+
+menu1 = Menu
+  "You are a superhero. What is your power?"
+  ["Spider-powers", "Invisibility", "I have a lot of money"]
+  [spiderman1, invisible1, batman1]
+
+spiderman1 = Menu
+  "You are Spider-Man, or at least a cheap rip-off of him.\nUsing your spider-senses, speed, and webs,\nyou fight crime and deliver pizzas in New York City.\nYou also used to have an Uncle Ben."
+  ["End"]
+  [endMenu]
+
+
+invisible1 = Menu
+  "You have the power of invisibility,\nwhich could be very dangerous in the wrong hands.\nTry not to get into too much trouble."
+  ["End"]
+  [endMenu]
+  
+batman1 = Menu
+  "You are Batman. You have a lot of money,\nbut you can't buy your parents back to life.\nYou have a lot of gadgets you use to fight criminals."
+  ["End"]
+  [endMenu]
